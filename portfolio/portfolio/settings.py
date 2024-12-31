ff6aea74da73db49a2af367158dca53823eadde4
Abdul Hasib <136143027+abdulHasib2030@users.noreply.github.com>
2024-12-31 19:29:39 +0600

    Update settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -124,12 +124,4 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-######## EMAIL PASSWORD another Secure File.. ########
-EMAIL_HOST_USER = 'abdulhasib2030@gmail.com'
-EMAIL_HOST_PASSWORD = 'bxhhmwpormjcygqt'
-EMAIL_USE_TLS = True
 
